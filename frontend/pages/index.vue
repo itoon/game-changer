4b5ad98020592c3a3cd278914321b35d1c074a1a
Songklod Saengvoratip <songklod@marvelous.studio>
2024-08-30 00:54:46 +0700

    fix: design and question answer

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,89 +1,73 @@
 <template>
-  <div class="bg-[#2961e2]">
+  <div class="bg-main">
     <nav
       class="absolute w-full p-4 z-10 flex items-center justify-center md:justify-between gap-x-1 lg:gap-x-10 md:px-[120px] px-4 py-2 transition-all"
     >
-      <div class="w-full flex gap-x-2 lg:px-2">
-        <div class="flex justify-center items-center text-white md:gap-4">
-          <NuxtImg
-            src="https://www.think-digital.app/icons/icon-arrow-small-left-white.svg"
-            class="w-6"
-          />
-          <p class="md:text-lg">กลับ</p>
-        </div>
+      <div class="flex w-full max-w-5xl mx-auto gap-x-2 lg:px-2">
         <div class="flex-1 flex gap-2 py-[20px] md:px-20">
-          <div
-            class="w-full overflow-hidden bg-[#00216c] rounded-full"
-            v-for="x in 8"
-          >
+          <div class="w-full overflow-hidden bg-[#016789] rounded-full">
             <div
               class="h-2 transition-all bg-white rounded-full"
-              style="width: 25%"
+              :style="{
+                width: `${
+                  ((answerTypeQuestions + answerCharacterQuestions) /
+                    totalQuestions) *
+                  100
+                }%`,
+              }"
             ></div>
           </div>
-        </div>
-        <div class="flex justify-center items-center text-white md:text-lg">
-          1/30
         </div>
       </div>
     </nav>
     <div class="min-h-screen">
       <main
-        class="container max-w-[1366px] px-4 xl:px-[103px] mx-auto gap-x-6 h-full pt-16 pb-4 flex flex-col"
+        class="container max-w-5xl px-4 xl:px-[103px] mx-auto gap-x-6 h-full pt-16 pb-4 flex flex-col md:pt-32"
       >
         <div
-          class="inline-flex flex-col gap-2 h-fit col-span-full md:col-start-4 md:col-end-10 py-4 md:py-6 md:gap-4"
+          class="px-4 py-6 bg-white rounded-3xl min-h-[75vh] md:py-20 md:px-20 md:min-h-0"
         >
-          <img
-            :src="renderQuestion.image"
-            class="object-contain w-auto h-20 mx-auto"
-          />
-          <p class="text-center font-bold text-white text-lg md:text-xl">
-            {{ renderQuestion.text }}
-          </p>
-        </div>
-        <div class="grid grid-cols-12 gap-4 w-full max-w-3xl mx-auto md:gap-6">
           <div
-            class="col-span-6 md:col-span-3"
-            v-for="(choice, index) in renderQuestion.choices"
-            :key="index"
+            class="inline-flex flex-col gap-2 h-fit col-span-full md:py-6 md:gap-4 md:flex md:justify-between lg:py-0"
           >
-            <button
-              class="relative flex flex-col items-center w-full gap-2 p-4 transition-all rounded-lg cursor-pointer select-none blank"
-              @click="selected(choice)"
-              :class="{
-                'bg-[#dde6fd]': select === choice,
-                'bg-white': select !== choice,
-              }"
+            <p class="text-lg font-bold text-center lg:text-3xl">
+              {{ renderQuestion.question }}
+            </p>
+          </div>
+          <div class="grid w-full grid-cols-12 gap-4 mx-auto mt-6 md:gap-6">
+            <div
+              class="col-span-12 md:col-span-12"
+              v-for="(choice, index) in renderChoices"
+              :key="index"
             >
-              <NuxtImg
-                v-if="choice.image"
-                :src="choice.image"
-                class="w-20 h-20"
-              />
-              <p>{{ choice.text }}</p>
-
-              <div
-                class="w-6 h-6 rounded-full bg-[#dde6fd] absolute top-2 right-2"
-                v-if="select !== choice"
-              ></div>
-              <div class="w-6 h-6 absolute top-1 right-3" v-else>
-                <Icon
-                  name="ph:check-circle-fill"
-                  size="32"
-                  class="text-[#2961e2]"
-                />
-              </div>
-            </button>
+              <button
+                class="relative flex flex-col items-center w-full gap-2 px-4 py-3 text-sm transition-all rounded-lg cursor-pointer select-none blank border-choice lg:text-lg"
+                @click="selected(choice)"
+                :class="{ 'choice-selected': select === choice }"
+              >
+                <p>{{ choice.text }}</p>
+              </button>
+            </div>
           </div>
         </div>
       </main>
-      <footer class="bg-white absolute bottom-0 w-full">
+      <footer class="absolute bottom-0 w-full bg-white" v-show="step == 1">
         <div class="container p-4 mx-auto md:flex md:justify-end">
           <button
-            class="bg-[#2961e2] disabled:bg-[#b0bec5] rounded-lg text-white w-full z-10 text-center min-h-[40px] md:min-h-[59px] px-2 min-w-[100px] md:w-full md:max-w-[172px] md:min-w-[172px] md:!leading-[150%] md:!py-4 transition-all duration-75 disabled btn-primary md:!h-14 font-bold"
+            class="bg-black disabled:bg-[#b0bec5] rounded-lg text-white w-full z-10 text-center min-h-[40px] md:min-h-[59px] px-2 min-w-[100px] md:w-full md:max-w-[172px] md:min-w-[172px] md:!leading-[150%] md:!py-4 transition-all duration-75 disabled btn-primary md:!h-14 font-bold"
             :disabled="select === null"
-            @click="next"
+            @click="submit1Step"
+          >
+            ต่อไป
+          </button>
+        </div>
+      </footer>
+      <footer class="absolute bottom-0 w-full bg-white" v-show="step == 2">
+        <div class="container p-4 mx-auto md:flex md:justify-end">
+          <button
+            class="bg-black disabled:bg-[#b0bec5] rounded-lg text-white w-full z-10 text-center min-h-[40px] md:min-h-[59px] px-2 min-w-[100px] md:w-full md:max-w-[172px] md:min-w-[172px] md:!leading-[150%] md:!py-4 transition-all duration-75 disabled btn-primary md:!h-14 font-bold"
+            :disabled="select === null"
+            @click="submit2Step"
           >
             ต่อไป
           </button>
@@ -94,54 +78,124 @@
 </template>
 
 <script setup lang="ts">
-const select = ref(null);
-const index = ref(0);
-const choices = [
-  {
-    image:
-      "https://storage.googleapis.com/third-apex-370513.appspot.com/images/assessment/E-yDSNMvxE0o8pMXSSekn.png",
-    text: "เห็นด้วยที่สุด",
-  },
-  {
-    image:
-      "https://storage.googleapis.com/third-apex-370513.appspot.com/images/assessment/E-yDSNMvxE0o8pMXSSekn.png",
-    text: "ไม่เห็นด้วยที่สุด",
-  },
-  {
-    image:
-      "https://storage.googleapis.com/third-apex-370513.appspot.com/images/assessment/E-yDSNMvxE0o8pMXSSekn.png",
-    text: "เห็นด้วยที่สุด",
-  },
-  {
-    image:
-      "https://storage.googleapis.com/third-apex-370513.appspot.com/images/assessment/E-yDSNMvxE0o8pMXSSekn.png",
-    text: "ไม่เห็นด้วยที่สุด",
-  },
-];
-const question = ref([
-  {
-    image:
-      "https://storage.googleapis.com/third-apex-370513.appspot.com/images/assessment/W-yeqJAIbRMwTKGX4Kx8b.png",
-    text: "ฉันกล้าจะทำสิ่งที่ไม่น่ารักบนโลกออนไลน์มากกว่าในชีวิตจริง",
-    choices: choices,
-  },
-  {
-    image:
-      "https://storage.googleapis.com/third-apex-370513.appspot.com/images/assessment/W-yeqJAIbRMwTKGX4Kx8b.png",
-    text: "ฉันกล้าจะทำสิ่งที่ไม่น่ารักบนโลกออนไลน์มากกว่าในชีวิตจริง",
-    choices: choices,
-  },
-]);
-const renderQuestion = computed(() => question.value[index.value]);
-const answers = ref(new Array(question.value.length).fill(null));
+type TypeAnswer =
+  | "Action"
+  | "People"
+  | "Thought"
+  | "Shaper"
+  | "Implementer"
+  | "Completer Finisher"
+  | "Coordinator"
+  | "Team worker"
+  | "Resource Investigator"
+  | "Plant"
+  | "Monitor Evaluator"
+  | "Specialist";
 
-const selected = (choice: any) => {
+const select = ref<{
+  text: string;
+  value: TypeAnswer;
+} | null>(null);
+const step = ref(1);
+const index = ref(0);
+import typeQuestions from "~/data/type.json";
+import characterQuestions from "~/data/character.json";
+// order typeQuestion from type.json
+const questions = ref(typeQuestions.sort(() => Math.random() - 0.5));
+// const questions =
+const renderQuestion = computed(() => questions.value[index.value]);
+const renderChoices = computed(() => {
+  const answers = [...renderQuestion.value.answers];
+  return answers.sort(() => Math.random() - 0.5);
+});
+
+const selected = (choice: { text: string; value: TypeAnswer }) => {
   select.value = choice;
 };
 
-const next = () => {
-  answers.value[index.value] = select.value;
-  index.value++;
-  select.value = null;
+const totalQuestions = 8;
+const answerTypeQuestions = ref(0);
+const answerCharacterQuestions = ref(0);
+
+const typeAnswers = ref({
+  Action: 0,
+  People: 0,
+  Thought: 0,
+});
+
+const submit1Step = () => {
+  if (
+    select.value?.value == "Action" ||
+    select.value?.value == "People" ||
+    select.value?.value == "Thought"
+  ) {
+    typeAnswers.value[select.value.value] += 1;
+    if (typeAnswers.value[select.value.value] >= 2) {
+      step.value = 2;
+      questions.value = characterQuestions[select.value.value].sort(
+        () => Math.random() - 0.5
+      );
+      index.value = 0;
+      answerTypeQuestions.value = 4;
+    } else {
+      index.value++;
+      answerTypeQuestions.value++;
+    }
+    select.value = null;
+  }
+};
+
+const characterAnswers = ref({
+  Shaper: 0,
+  Implementer: 0,
+  "Completer Finisher": 0,
+  Coordinator: 0,
+  "Team worker": 0,
+  "Resource Investigator": 0,
+  Plant: 0,
+  "Monitor Evaluator": 0,
+  Specialist: 0,
+});
+
+const router = useRouter();
+
+const submit2Step = () => {
+  if (
+    select.value?.value == "Shaper" ||
+    select.value?.value == "Implementer" ||
+    select.value?.value == "Completer Finisher" ||
+    select.value?.value == "Coordinator" ||
+    select.value?.value == "Team worker" ||
+    select.value?.value == "Resource Investigator" ||
+    select.value?.value == "Plant" ||
+    select.value?.value == "Monitor Evaluator" ||
+    select.value?.value == "Specialist"
+  ) {
+    characterAnswers.value[select.value.value] += 1;
+    if (characterAnswers.value[select.value.value] >= 2) {
+      answerCharacterQuestions.value = 4;
+      router.push("/result?type=" + select.value.value);
+    } else {
+      index.value++;
+      answerCharacterQuestions.value++;
+    }
+    select.value = null;
+  }
 };
 </script>
+
+<style lang="css" scoped>
+.bg-main {
+  background: linear-gradient(168.63deg, #4bb5e6 2.44%, #19c768 98.03%);
+}
+.border-choice {
+  border: 2px solid var(--linear, #4bb5e6);
+}
+.choice-selected {
+  background: linear-gradient(
+    169deg,
+    rgba(75, 181, 230, 0.4) 2.44%,
+    rgba(25, 199, 104, 0.4) 98.03%
+  );
+}
+</style>
---
+++ frontend/pages/result.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="bg-[#072462]">
+    <nav
+      class="absolute w-full p-4 z-10 flex items-center justify-center md:justify-between gap-x-1 lg:gap-x-10 md:px-[120px] px-4 py-2 transition-all"
+    >
+      <div class="flex w-full gap-x-2 lg:px-2">
+        <div class="flex-1 flex gap-2 py-[20px] md:px-20">
+          <div class="w-full overflow-hidden bg-[#01102E] rounded-full">
+            <div
+              class="h-2 transition-all bg-[#3C6AA5] rounded-full"
+              style="width: 100%"
+            ></div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="min-h-screen">
+      <main
+        class="container max-w-[1366px] px-4 xl:px-[103px] mx-auto gap-x-6 h-full pt-16 pb-4 flex flex-col"
+      >
+        <div class="px-4 py-6 bg-white rounded-3xl min-h-[75vh]">
+          <div
+            class="flex flex-col justify-between gap-2 col-span-full md:col-start-4 md:col-end-10 md:py-6 md:gap-4"
+          >
+            <p class="mt-5 mb-5 text-lg font-bold text-center md:text-xl">
+              และตัวละครของคุณก็คือ....
+            </p>
+            <NuxtImg
+              v-if="!loading"
+              :src="'characters/' + $route.query.type + '.png'"
+              class="w-full"
+            />
+          </div>
+        </div>
+      </main>
+      <footer class="absolute bottom-0 w-full bg-white">
+        <div class="container p-4 mx-auto md:flex md:justify-end">
+          <button
+            class="bg-black disabled:bg-[#b0bec5] rounded-lg text-white w-full z-10 text-center min-h-[40px] md:min-h-[59px] px-2 min-w-[100px] md:w-full md:max-w-[172px] md:min-w-[172px] md:!leading-[150%] md:!py-4 transition-all duration-75 disabled btn-primary md:!h-14 font-bold"
+          >
+            Save to Line
+          </button>
+        </div>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const loading = ref(true);
+setTimeout(() => {
+  loading.value = false;
+}, 1000);
+</script>
+
+<style lang="css" scoped>
+.bg-main {
+  background: linear-gradient(168.63deg, #4bb5e6 2.44%, #19c768 98.03%);
+}
+.border-choice {
+  border: 2px solid var(--linear, #4bb5e6);
+}
+.choice-selected {
+  background: linear-gradient(
+    169deg,
+    rgba(75, 181, 230, 0.4) 2.44%,
+    rgba(25, 199, 104, 0.4) 98.03%
+  );
+}
+</style>
