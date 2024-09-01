e78f18e5f957f9884793a7d8927dfbd14adaa80d
Songklod Saengvoratip <songklod@marvelous.studio>
2024-09-01 16:45:21 +0700

    fix: design and question answer

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,19 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   ssr: false,
+  app: {
+    baseURL: "/game-changer",
+    buildAssetsDir: "assets",
+    head: {
+      title: "Game Changer x Spark Education",
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/game-changer/favicon.ico",
+        },
+      ],
+    },
+  },
   modules: ["@nuxtjs/tailwindcss", "@nuxt/image", "@nuxt/icon"],
 });
